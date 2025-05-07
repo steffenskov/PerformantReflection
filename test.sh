f271c038f444cde0b1c0621eefe34fe617e16251
Steffen Skov <steffen@ckode.dk>
2025-05-07 17:19:45 +0200

    chore: test script (#68)

--- test.sh
+++ test.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
+rm TestResults -rf 2> /dev/null
+rm coverage-report -rf 2> /dev/null
 
-rm -rf tests/PerformantReflection.UnitTests/TestResults
-dotnet build
-dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings
+dotnet test --results-directory ./TestResults --collect:"XPlat Code Coverage"
+
+
+coverage_file=$(find . -name 'coverage.cobertura.xml' -print -quit)
+
+# Check if the file exists and move it
+if [[ -n "$coverage_file" ]]; then
+  mv "$coverage_file" .
+  echo "Moved coverage file to the current directory."
+else
+  echo "Coverage file not found."
+fi
+
+
+reportgenerator -reports:coverage.cobertura.xml -targetdir:coverage-report -reporttypes:Html
+rm coverage.cobertura.xml
+rm TestResults -rf 2> /dev/null
+
+xdg-open coverage-report/index.htm
