d57182f59c94ed6b34ffcb951c3a9c8c93dae844
steffenskov <steffen@ckode.dk>
2025-10-18 14:41:16 +0200

    chore: Updated test.sh to handle multiple test projects

--- test.sh
+++ test.sh
@@ -6,18 +6,7 @@ rm coverage-report -rf 2> /dev/null
 dotnet test --results-directory ./TestResults --collect:"XPlat Code Coverage"
 
 
-coverage_file=$(find . -name 'coverage.cobertura.xml' -print -quit)
-
-# Check if the file exists and move it
-if [[ -n "$coverage_file" ]]; then
-  mv "$coverage_file" .
-  echo "Moved coverage file to the current directory."
-else
-  echo "Coverage file not found."
-fi
-
-
-reportgenerator -reports:coverage.cobertura.xml -targetdir:coverage-report -reporttypes:Html
+reportgenerator -reports:"TestResults/*/coverage.cobertura.xml" -targetdir:coverage-report -reporttypes:Html
 rm coverage.cobertura.xml
 rm TestResults -rf 2> /dev/null
 
