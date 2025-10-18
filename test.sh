dfafd3dc1f30d51c9f592413f8c62a3eb28616ea
steffenskov <steffen@ckode.dk>
2025-10-18 14:44:20 +0200

    chore: Removed obsolete line in test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,6 @@ dotnet test --results-directory ./TestResults --collect:"XPlat Code Coverage"
 
 
 reportgenerator -reports:"TestResults/*/coverage.cobertura.xml" -targetdir:coverage-report -reporttypes:Html
-rm coverage.cobertura.xml
 rm TestResults -rf 2> /dev/null
 
 xdg-open coverage-report/index.htm
